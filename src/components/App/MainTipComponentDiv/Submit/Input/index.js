6f9d9395cddbcdd8d7d6ad949b6efd08ed35bd6f
michaelfswann <mikeswann@mail.com>
2020-11-09 17:02:33 +0000

    POST req.body undefined

--- src/components/App/MainTipComponentDiv/Submit/Input/index.js
+++ src/components/App/MainTipComponentDiv/Submit/Input/index.js
@@ -1,44 +1,48 @@
 import React, { useEffect, useState } from "react";
 
-function Input(){
-    const [newTip, setNewTip] = useState("")
-    const [clicked, setClicked] = useState(false);
+function Input() {
+  const [newTip, setNewTip] = useState("");
+  const [clicked, setClicked] = useState(false);
 
-    useEffect(() => {
-        if(clicked){
-            async function submitTip(){
-                const res = await fetch(
-                    `https://localhost:5000/createTip`,
-                    { method: `POST`,
-                      headers: { accept: "application/json" },
-                      body: JSON.stringify({tip: newTip})
-                    }
-                  )
-                  const data = await res.json();
-                if (data.success){
-                    console.log("WOOT YOU SUBMITTED A TIP!!")
-                }
-            };
-            submitTip();
-            setClicked(false);
+  useEffect(() => {
+    if (clicked) {
+      async function submitTip() {
+        console.log(newTip + " 10");
+        const requestOptions = {
+          method: `POST`,
+          headers: { accept: "application/json" },
+          body: newTip,
+        };
+
+        const req = await fetch(
+          `http://localhost:5000/createTip`,
+          requestOptions
+        );
+        const data = await req.json();
+        if (data.success) {
+          console.log(data);
+          console.log("WOOT YOU SUBMITTED A TIP!!");
         }
-    }, [clicked, newTip])
-
-    function handleChange(event){
-        setNewTip(event.target.value);
+      }
+      submitTip();
+      setClicked(false);
     }
+  }, [clicked, newTip]);
 
-    function handleClick(){
-        setClicked(true);
-    }
+  function handleChange(event) {
+    setNewTip(event.target.value);
+  }
 
-    
-    return(
-        <div>
-            <input onChange={handleChange} placeholder="write tip here"></input>
-            <button onClick={handleClick}>Submit Tip</button>
-        </div>
-    )
+  function handleClick() {
+    setClicked(true);
+  }
+
+  return (
+    <div>
+      <input onChange={handleChange} placeholder="write tip here"></input>
+      <button onClick={handleClick}>Submit Tip</button>
+    </div>
+  );
 }
 
 export default Input;
