49171f23ae31eea7715b098c58b4a7e20da8990d
= <beckyljones151192@gmail.com>
2020-11-10 14:34:56 +0000

    react route done

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,3 +1,5 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import MainTipComponentDiv from "./MainTipComponentDiv/Display";
 import Input from "./MainTipComponentDiv/Submit/Input";
 // Main CSS
@@ -10,19 +12,36 @@ import "./Animations.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img
-          src="/images/plusMentor/plusMentor_gradient.png"
-          alt="plusMentor Logo"
-          width="350"
-        />
-        <br />
-        <Input />
-        <br />
-        <MainTipComponentDiv />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img
+            src="/images/plusMentor/plusMentor_gradient.png"
+            alt="plusMentor Logo"
+            width="350"
+          />
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/createTip">Create Tip</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+
+        <Switch>
+          <Route path="/createTip">
+            <Input />
+          </Route>
+          <Route path="/">
+            <MainTipComponentDiv />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
